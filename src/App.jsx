8f30261bfc9981ad65397d6094a5aa5c6ba06f42
dbslima <dbslima@fornecedor.qualicorp.com.br>
2025-10-20 12:12:13 -0300

    Add Toolbar component to App for enhanced functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Timeline from './components/Timeline';
+import Toolbar from './components/Toolbar';
 
 function App() {
   return (
     <div className="app">
       <Timeline />
+      <Toolbar />
     </div>
   );
 }
